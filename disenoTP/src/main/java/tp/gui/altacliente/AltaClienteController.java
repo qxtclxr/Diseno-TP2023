70fa7bf4b25b047cf98a3b51401bd0242a00a542
WIN10 <WIN10@DESKTOP-M3H7AR3>
2023-11-10 12:57:35 -0300

    Se avanzo con AltaCliente (Controlador y UI)

--- disenoTP/src/main/java/tp/gui/altacliente/AltaClienteController.java
+++ disenoTP/src/main/java/tp/gui/altacliente/AltaClienteController.java
@@ -1,5 +1,50 @@
 package tp.gui.altacliente;
 
+
+import java.util.Date;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
 public class AltaClienteController {
 
+	@FXML
+	private TextField apellido;
+	@FXML
+	private TextField nombre;
+	@FXML
+	private ComboBox<?> tipoDocumento;
+	@FXML
+	private ComboBox<?> condicionIva;
+	@FXML
+	private ComboBox<?> sexo;
+	@FXML
+	private TextField nroDoc;
+	@FXML
+	private TextField cuil;
+	@FXML
+	private TextField correoElectronico;
+	@FXML
+	private TextField profesion;
+	@FXML
+	private Date fechaNacimiento;
+	@FXML
+	private TextField calle;
+	@FXML
+	private TextField piso;
+	@FXML
+	private TextField depto;
+	@FXML
+	private ComboBox<?> provincia;
+	@FXML
+	private ComboBox<?> localidad;
+	@FXML
+	private ComboBox<?> pais;
+	@FXML	
+	private Button darDeAltaCliente;
+	
+	public void holaMundo() {
+		apellido.appendText("HolaMundo!");
+	}
+	
 }
