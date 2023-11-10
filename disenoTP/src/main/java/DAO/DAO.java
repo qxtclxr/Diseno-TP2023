9eeda64ec8913f94b12f60369e6cbbd4c71d58b8
franp <franp@DESKTOP-4U0E43M>
2023-11-09 23:29:28 -0300

    Agrego Dao generico

---
+++ disenoTP/src/main/java/DAO/DAO.java
@@ -0,0 +1,13 @@
+package DAO;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface DAO <T> {
+	Optional<T> getById(Long id);
+	void saveInstance(T instance);
+	void updateInstance(T instance);
+	void deleteInstance(T instance);
+	List<T> getAll();
+
+}
---
+++ disenoTP/src/main/java/utiles/EntityManagerUtil.java
@@ -0,0 +1,20 @@
+package utiles;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class EntityManagerUtil {
+	public static EntityManager getEntityManager() {
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("pruebaTP");
+		EntityManager manager= factory.createEntityManager();
+		return manager;
+	}
+	/*
+	public static void main(String[] args) {
+		EntityManager manager=EntityManagerUtil.getEntityManager();
+		System.out.println("EntityManager class: "+ manager.getClass().getCanonicalName());
+	}
+	*/
+	
+}
