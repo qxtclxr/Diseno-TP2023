a006d90314e56e575cd2bf044fdfd0f426ea18d9
franp <franp@DESKTOP-4U0E43M>
2023-11-18 00:44:25 -0300

    van los get by patente, motor y chasis

--- disenoTP/src/main/java/tp/dao/PolizaDAO.java
+++ disenoTP/src/main/java/tp/dao/PolizaDAO.java
@@ -1,5 +1,13 @@
 package tp.dao;
 
+import java.util.List;
+
+import jakarta.persistence.TypedQuery;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.ParameterExpression;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
 import tp.entidad.Poliza;
 
 public class PolizaDAO extends AbstractDAO<Poliza> {
@@ -8,14 +16,44 @@ public class PolizaDAO extends AbstractDAO<Poliza> {
 	}
 	
 	public List<Poliza> getByMotor(String motor){
+		CriteriaBuilder cbuild=this.getEntityManager().getCriteriaBuilder();
+		CriteriaQuery<Poliza> cQuery =cbuild.createQuery(Poliza.class);
+		Root<Poliza> root=cQuery.from(Poliza.class);
 		
+		Predicate p = cbuild.conjunction();
+		ParameterExpression<String> motorP= cbuild.parameter(String.class,"motor");
+		p=cbuild.and(p,cbuild.equal(root.get("vehiculoAsegurado").get("motor"), motorP));
+		cQuery.where(p);
+		TypedQuery<Poliza> tq = this.getEntityManager().createQuery(cQuery).setParameter("motor", motor);
+		List<Poliza> ret = tq.getResultList();
+		return ret;
 	}
 	
 	public List<Poliza> getByChasis(String chasis){
+		CriteriaBuilder cbuild=this.getEntityManager().getCriteriaBuilder();
+		CriteriaQuery<Poliza> cQuery =cbuild.createQuery(Poliza.class);
+		Root<Poliza> root=cQuery.from(Poliza.class);
 		
+		Predicate p = cbuild.conjunction();
+		ParameterExpression<String> chasisP= cbuild.parameter(String.class,"chasis");
+		p=cbuild.and(p,cbuild.equal(root.get("vehiculoAsegurado").get("chasis"), chasisP));
+		cQuery.where(p);
+		TypedQuery<Poliza> tq = this.getEntityManager().createQuery(cQuery).setParameter("chasis", chasis);
+		List<Poliza> ret = tq.getResultList();
+		return ret;
 	}
 	
 	public List<Poliza> getByPatente(String patente){
+		CriteriaBuilder cbuild=this.getEntityManager().getCriteriaBuilder();
+		CriteriaQuery<Poliza> cQuery =cbuild.createQuery(Poliza.class);
+		Root<Poliza> root=cQuery.from(Poliza.class);
 		
+		Predicate p = cbuild.conjunction();
+		ParameterExpression<String> patenteP= cbuild.parameter(String.class,"patente");
+		p=cbuild.and(p,cbuild.equal(root.get("vehiculoAsegurado").get("patente"), patenteP));
+		cQuery.where(p);
+		TypedQuery<Poliza> tq = this.getEntityManager().createQuery(cQuery).setParameter("patente", patente);
+		List<Poliza> ret = tq.getResultList();
+		return ret;
 	}
 }
