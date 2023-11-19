a9a3986142f708728ac1cc8d8d56806c58760941
franp <franp@DESKTOP-4U0E43M>
2023-11-19 12:45:42 -0300

    Agrego el generador de numeros de cliente en secuencia

--- disenoTP/src/main/java/tp/dao/PaisDAO.java
+++ disenoTP/src/main/java/tp/dao/PaisDAO.java
@@ -2,11 +2,13 @@ package tp.dao;
 
 import java.util.Optional;
 
+import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.TypedQuery;
 import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.CriteriaQuery;
 import jakarta.persistence.criteria.Root;
 import tp.entidad.Pais;
+import tp.entidad.Sucursal;
 
 public class PaisDAO extends AbstractDAO<Pais> {
 	public PaisDAO() {
@@ -20,5 +22,24 @@ public class PaisDAO extends AbstractDAO<Pais> {
 		cq.select(from).where(cb.equal(from.get("nombre"), nombre));
 		TypedQuery<Pais> query =this.getEntityManager().createQuery(cq);
 		return Optional.ofNullable(query.getSingleResult());
+	}
+	
+	public long getSerialCliente(Pais p) {
+		long ret;
+		EntityTransaction t= this.getEntityManager().getTransaction();
+		try {
+			t.begin();
+			p=this.getById(p.getIdPais()).get();
+			
+			ret=p.getSecuenciaCliente();
+			p.setSecuenciaCliente(ret+1);;
+			this.getEntityManager().persist(p);
+			t.commit();
+		} catch(RuntimeException exep) {
+			t.rollback();
+			throw exep;
+		}
+		return ret;
+		
 	}
 }
