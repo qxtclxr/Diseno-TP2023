210c3ea93dbc34e491c5e8fae2352630c0abffa4
franp <franp@DESKTOP-4U0E43M>
2023-11-22 23:51:11 -0300

    Va el buscar cliente flexibilizado

--- disenoTP/src/main/java/tp/dao/ClienteDAO.java
+++ disenoTP/src/main/java/tp/dao/ClienteDAO.java
@@ -2,15 +2,19 @@ package tp.dao;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.ArrayList;
 
 
 import jakarta.persistence.Query;
 import jakarta.persistence.TypedQuery;
 import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Expression;
+import jakarta.persistence.criteria.Predicate;
 import jakarta.persistence.criteria.Root;
 import tp.entidad.Cliente;
 import tp.entidad.TipoDocumento;
+
 
 public class ClienteDAO extends AbstractDAO<Cliente> {
 	public ClienteDAO() {
@@ -27,7 +31,7 @@ public class ClienteDAO extends AbstractDAO<Cliente> {
 		return Optional.ofNullable(query.getSingleResult());
 	}
 	
-	public List<Cliente> buscarCliente(String nroCliente, String apellido, String nombre, TipoDocumento tipoDoc, String nroDoc){
+	public List<Cliente> buscarCliente_legacy(String nroCliente, String apellido, String nombre, TipoDocumento tipoDoc, String nroDoc){
 		CriteriaBuilder cbuilder = this.getEntityManager().getCriteriaBuilder();
 	    CriteriaQuery<Cliente> cQuery = cbuilder.createQuery(Cliente.class);
 	    Root<Cliente> from = cQuery.from(Cliente.class);
@@ -52,4 +56,56 @@ public class ClienteDAO extends AbstractDAO<Cliente> {
 	    
 	    
 	}
+	
+	public List<Cliente> buscarCliente(String nroCliente, String[] apellidoTokens, String[] nombreTokens, TipoDocumento tipoDoc, String nroDoc){
+	    CriteriaBuilder cbuilder = this.getEntityManager().getCriteriaBuilder();
+	    CriteriaQuery<Cliente> cQuery = cbuilder.createQuery(Cliente.class);
+	    Root<Cliente> from = cQuery.from(Cliente.class);
+	    cQuery.select(from);
+
+	    List<Predicate> predicates = new ArrayList<Predicate>();
+
+	    if (!(nombreTokens.length==0)) {
+	        // Busca coincidencias parciales
+	       // String[] nombreTokens = StringUtils.split(nombre);
+	        Expression<String> nombreExp = from.get("nombres");
+	        for (String token : nombreTokens) {
+	            predicates.add(cbuilder.like(nombreExp, "%" + token + "%"));
+	        }
+	    }
+
+	    if (!(apellidoTokens.length==0)) {
+	        // Busca coincidencias parciales
+	        //String[] apellidoTokens = StringUtils.split(apellido);
+	        Expression<String> apellidoExp = from.get("apellido");
+	        for (String token : apellidoTokens) {
+	            predicates.add(cbuilder.like(apellidoExp, "%" + token + "%"));
+	        }
+	    }
+
+	    if (!nroCliente.equals("")) {
+	        // Busca coincidencias parciales en el número de cliente
+	        predicates.add(cbuilder.like(from.get("nroCliente"), "%" + nroCliente + "%"));
+	    }
+
+	    if (!nroDoc.equals("") && tipoDoc != null) {
+	        // Busca coincidencias parciales en el número de documento y en el tipo de documento
+	        predicates.add(cbuilder.equal(from.get("tipoDocumento"), tipoDoc));
+	        predicates.add(cbuilder.like(from.get("nroDocumento"), "%" + nroDoc + "%"));
+	    } else if (!nroDoc.equals("")) {
+	        // Si no se proporciona un tipo de documento, solo busca por número de documento
+	        predicates.add(cbuilder.like(from.get("nroDocumento"), "%" + nroDoc + "%"));
+	    } else if (tipoDoc != null) {
+	        // Si no se proporciona un número de documento, solo busca por tipo de documento
+	        predicates.add(cbuilder.equal(from.get("tipoDocumento"), tipoDoc));
+	    }
+
+	    // Combina todas las condiciones con un AND
+	    cQuery.where(predicates.toArray(new Predicate[0]));
+
+	    TypedQuery<Cliente> tq = this.getEntityManager().createQuery(cQuery);
+	    List<Cliente> ret = tq.getResultList();
+	    return ret;
+	}
+
 }
